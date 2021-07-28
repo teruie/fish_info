d51b3a59d32dd726816fd14153e0d095cd6cdec2
teruie <teruie0817@icloud.com>
2021-07-28 18:16:28 +0900

    modelの作成

--- app/fish/admin.py
+++ app/fish/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from fish.models import Article, Category, Place, Profile
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Article)
+admin.site.register(Category)
+admin.site.register(Place)
