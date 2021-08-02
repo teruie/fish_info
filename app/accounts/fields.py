39907ce7b097dcee397990ad858d23502ef5fa12
teruie <teruie0817@icloud.com>
2021-08-02 13:17:49 +0900

    search.htmlのデザイン変更

--- app/accounts/fields.py
+++ app/accounts/fields.py
@@ -3,8 +3,6 @@ from django.db import models
 
 
 class LowerCharField(models.CharField):
-    """
-    大文字を小文字に自動変換するフィールド。
-    """
+    
     def get_prep_value(self, value):
         return str(value).lower()
