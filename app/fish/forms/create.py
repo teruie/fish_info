f751051ee8dc48e6e1123149c8d5a3af89541eb9
teruie <teruie0817@icloud.com>
2021-09-01 10:57:33 +0900

    gitの更新

--- app/fish/forms/create.py
+++ app/fish/forms/create.py
@@ -8,6 +8,10 @@ class ArticleForm(forms.ModelForm):
         model = Article
         fields = ['fish_category', 'fishing_comment', 'fish_img','length','weight','place']
 
+        widgets = {
+            'fishing_comment': forms.Textarea(attrs={'class': 'text',}),
+        }
+
 
 class ProfileForm(forms.ModelForm):
     class Meta:
@@ -20,10 +24,16 @@ class CategoryForm(forms.ModelForm):
         model = Category
         fields = ['title']
 
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'textinputclass',}),
+        }
+
 
 class PlaceForm(forms.ModelForm):
     class Meta:
         model = Place
         fields = ['title']
 
-
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'textinputclass',}),
+        }
